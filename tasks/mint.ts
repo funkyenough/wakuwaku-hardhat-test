8cb1058efea13bd6aa24806684cbc8fd6ee7ed62
Naokiakazawa <naoki9akazawa@gmail.com>
2023-04-28 15:35:00 +0900

    naoki refactoring: fix mint task and add accounts task

--- tasks/mint.ts
+++ tasks/mint.ts
@@ -9,26 +9,31 @@ task(
     }
   );
 
+// yarn hardhat mint --contract 0x5fbdb2315678afecb367f032d93f642f64180aa3 --to 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 --network localhost
 let tokenId = 0;
 task("mint", "minting new tokenId")
     .addParam("contract", "nftContractAddr")
     .addParam("to", "address who get NFTs")
-    //.addParam("tokenid")
     .setAction(async(args, hre) =>{
-        // const contractAddress = args.contract;
-        // const publickkey = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199" //hardhat
-        // const privatekey = "0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e" //hardhat
-        // const PROVIDER_URL = "http://localhost:8545";
 
-        const NftContract = await hre.ethers.getContractAt("contract", args.contract);
-        const abi = await require("../artifacts/contracts/Nft.sol/Nft.json");
-
-
-        const contract = new hre.ethers.Contract(args.contract, abi, hre.ethers.getDefaultProvider());
-
-        const signer = await hre.ethers.getSigner("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199")
-        const tx = await contract.connect(signer).mintNFT(args.to, tokenId);
-
+        // lower cases
+        const NftContract = await hre.ethers.getContractAt("Nft", args.contract);
+        const tx = await NftContract.mintNFT(args.to, "http://example.com");
         await tx.wait();
 
+        console.log(`transaction hash: ${tx.hash}`);
+/*
+        const contract = new hre.ethers.Contract(args.contract, abi, hre.ethers.getDefaultProvider());
+        const signer = await hre.ethers.getSigner("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199")
+        const tx = await contract.connect(signer).mintNFT(args.to, tokenId);
+        await tx.wait();
+*/
     });
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
